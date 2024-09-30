b2f86314310cc1595ef9efb38e4fc75fb84b7b84
Bhavesh <bhavesh.khubnani@gmail.com>
2024-09-29 17:25:29 -0700

    Fixed resource path

--- app/src/main/java/com/ksmb/DiagnoseMeAI/configuration/GoogleCredentialsConfig.java
+++ app/src/main/java/com/ksmb/DiagnoseMeAI/configuration/GoogleCredentialsConfig.java
@@ -1,6 +1,8 @@
 package com.ksmb.DiagnoseMeAI.configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -12,19 +14,18 @@ import java.util.Arrays;
 
 @Configuration
 public class GoogleCredentialsConfig {
+    @Value("${safe.resource.path}")
+    String safeResourcePath;
 
     @Bean
     public GoogleCredentials googleCredentials() throws IOException {
-        Resource resource = new ClassPathResource("diagnosemeai-b5de0d5a7565.json");
-
-        // Open an input stream to the file
+        Resource resource = new ClassPathResource(safeResourcePath);
         InputStream in = resource.getInputStream();
 
         if (in == null) {
             throw new IOException("Credentials file not found");
         }
 
-        // Load Google credentials and set required scopes
         GoogleCredentials googleCredentials = GoogleCredentials.fromStream(in)
                 .createScoped(Arrays.asList(
                         "https://www.googleapis.com/auth/cloud-platform"
